a7b6486f0a3e0ca87480798ddcec97f8fe98062a
baeseongjae <3574214@naver.com>
2024-01-16 13:48:21 +0900

    ✨ feat: 할일 추가 기능 구현 - 컴포넌트 분리 X

--- 05-React-Vue-실습/bash/todolist/src/components/TodoList/TodoList.jsx
+++ 05-React-Vue-실습/bash/todolist/src/components/TodoList/TodoList.jsx
@@ -2,16 +2,27 @@ import { useState } from "react";
 
 const TodoList = () => {
   const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    if (input.trim() === "") {
+      alert("다시 입력하세요");
+      return;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+    };
+
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
 
   return (
     <>
-      <h1>TodoList</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(input);
-        }}
-      >
+      <h1>To Do List</h1>
+      <form onSubmit={addTodo}>
         <input
           value={input}
           type="text"
@@ -22,8 +33,9 @@ const TodoList = () => {
       </form>
       {/* 할일 목록 렌더링 */}
       <ul>
-        <li>할일1</li>
-        <li>할일2</li>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
       </ul>
     </>
   );
