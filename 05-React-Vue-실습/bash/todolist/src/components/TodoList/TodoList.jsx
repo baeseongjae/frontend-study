e8f738c50cc7c716969c37a335426e54bf09e60e
baeseongjae <3574214@naver.com>
2024-01-16 14:12:16 +0900

    ✨ feat: 할일 필터링 구현 - 컴포넌트 분리 X

--- 05-React-Vue-실습/bash/todolist/src/components/TodoList/TodoList.jsx
+++ 05-React-Vue-실습/bash/todolist/src/components/TodoList/TodoList.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 const TodoList = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("all");
 
+  // 할일 추가 로직 함수
   const addTodo = (e) => {
     if (input.trim() === "") {
       alert("다시 입력하세요");
@@ -19,12 +21,24 @@ const TodoList = () => {
     setInput("");
   };
 
+  // 진행상태 토글 로직
   const toggleTodo = (index) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === index ? { ...todo, completed: !todo.completed } : todo
     );
     setTodos(updatedTodos);
   };
+
+  // filter 상태변수에 따라 할일 필터링 진행
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "completed") {
+      return !todo.completed;
+    } else if (filter === "incompleted") {
+      return todo.completed;
+    } else {
+      return true;
+    }
+  });
 
   return (
     <>
@@ -38,9 +52,15 @@ const TodoList = () => {
         />
         <button>추가</button>
       </form>
+      <div>
+        <button onClick={() => setFilter("all")}>전체</button>
+        <button onClick={() => setFilter("incompleted")}>미완료</button>
+        <button onClick={() => setFilter("completed")}>완료</button>
+      </div>
+
       {/* 할일 목록 렌더링 */}
       <ul>
-        {todos.map((item) => (
+        {filteredTodos.map((item) => (
           <li
             key={item.id}
             style={{ textDecoration: item.completed ? "line-through" : "none" }}
