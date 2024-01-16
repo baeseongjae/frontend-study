4623c55a3812bc76524925faa8242a08d65d9d5d
baeseongjae <3574214@naver.com>
2024-01-16 23:36:34 +0900

    ✨ feat: (4) 할일 검색 기능 - 컴포넌트 분리

--- 05-React-Vue-실습/bash/todolist/src/components/TodoList.jsx
+++ 05-React-Vue-실습/bash/todolist/src/components/TodoList.jsx
@@ -1,14 +1,36 @@
-import classes from "./TodoList.module.css";
+import { useState } from "react";
 
+import classes from "./TodoList.module.css";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // 4. 할일 검색 기능
+  const searchTodos = () => {
+    if (search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) =>
+      todo.text.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
   return (
     <div className={classes.TodoList}>
       <h2>Today's Todos</h2>
-      <input type="text" placeholder="검색어를 입력하세요" />
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        onChange={handleChangeSearch}
+      />
       <ul>
-        {todos.map((todo) => (
+        {searchTodos().map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))}
       </ul>
